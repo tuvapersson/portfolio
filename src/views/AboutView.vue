c3a7375863e8926ff78a0faa91e69215cf928062
tuvapersson <80885269+tuvapersson@users.noreply.github.com>
2023-03-07 21:36:00 +0100

    spelling mistakes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -37,7 +37,7 @@ export default {
         <div class="flex-text">
         <p>My name is Tuva Persson, and I'm a front-end developer and UX/UI designer, currently finishing my studies at the Graphic Design & Web Development programme at Jönköping University. I would describe myself as a structured and creative person who always put alot of attention to the details. I always want to become better at what I'm doing, and I'm always looking to learn the next thing.</p>
         <p>I tend to lean more towards the minimalistic type of design – something you probably already know about me from looking at this portfolio.</p>
-        <p>My greatest interest is human-centered designs and coding, and I belive a combination of the two is the recipe for success. When it comes to development, I feel the most at home in the front end. I recently discovered my love for Vue.js, which I decided to use to build this portfolio. Although my main focuses are UX/UI design and web development, I also enjoy putting my creativity to the test in digital design, which I have worked with a lot, both in my studies and during my internship.</p>
+        <p>My greatest interests are human-centered designs and coding, and I believe a combination of the two is the recipe for success. When it comes to development, I feel the most at home in the front end. I recently discovered my love for Vue.js, which I decided to use to build this portfolio. Although my main focuses are UX/UI design and web development, I also enjoy putting my creativity to the test in digital design, which I have worked with a lot, both in my studies and during my internship.</p>
     </div>
     </div>
     <div class="flex-container" v-motion
